f01c3c0ce5f4956a0b9e5be6a5bd28bde48be548
Eddy32 <pedroritolima@gmail.com>
2021-01-16 18:32:19 +0000

    stream

--- server/src/sink.cpp
+++ server/src/sink.cpp
@@ -20,7 +20,6 @@
 
 
 
-
 // ZMQ
 void *sock_pull;
 void *sock_pub; 
@@ -188,7 +187,7 @@ void *send_in_thread(void *ptr)
         cv::vector<uchar> topic ;
         cv::imencode(".jpg", mat, buffer);
         printf("SIZE: %d \n",buffer.size());
-        zmq_send(stream_pub, to_string(i), 2, ZMQ_SNDMORE);
+        zmq_send(stream_pub, std::to_string(i), 2, ZMQ_SNDMORE);
         zmq_send(stream_pub, buffer.data(), buffer.size(), ZMQ_NOBLOCK);
       }
 
