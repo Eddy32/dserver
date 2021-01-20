e46ea380ff98932f67a54fc193fe6deb78a5131f
Eddy32 <pedroritolima@gmail.com>
2021-01-20 11:18:26 +0000

    packets

--- server/src/packet.cpp
+++ server/src/packet.cpp
@@ -3,7 +3,7 @@
 #include <json-c/json.h>	// for json
 #include <cstring> // for memcpy
 #include <stdio.h>
-#include "base64.h"
+#include "base64.hpp"
 #include "packet.hpp"
 #include <iostream>
 
@@ -15,11 +15,12 @@ Packet::Packet()
 {
 };
 
-Packet::Packet(int user, int camera, std::time_t ts, vector<Mat> dados )
+Packet::Packet(std::string user, std::string camera, std::time_t ts, vector<Mat> dados, vector<std::string> dados_classes )
 {
   id_user = user;
   id_camera = camera;
   timestamp = ts;
+  classes = dados_classes;
   frames = dados;
 };
 
@@ -38,6 +39,7 @@ int packet_to_json(unsigned char* buf, Packet* pacote)
   vector<uchar> buff;
 
   string data;
+  string dataclasses = "";
   for (Mat frame : pacote->frames){
     imencode(".jpg", frame, buff, param);
     unsigned char* convertido = &buff[0];
@@ -46,10 +48,19 @@ int packet_to_json(unsigned char* buf, Packet* pacote)
     data.append("\"");
     data.append(",");
   }
+  for (string classe : pacote->classes){
+    dataclasses.append("\"");
+    dataclasses.append(classe);
+    dataclasses.append("\"");
+    dataclasses.append(",");
+  }
+  if ( pacote->classes.size() > 0 )
+    dataclasses.pop_back(); // eliminar ultimo ,
   data.pop_back(); // eliminar ultimo ,
   ss << "{\n\"id_user\":\"" << pacote->id_user << "\",\n"
          << "\"id_camera\":\"" << pacote->id_camera << "\",\n"
          << "\"timestamp\":\"" << pacote->timestamp << "\",\n"
+         << "\"classes\":[" << dataclasses << "],\n"
          << "\"dados\":[" << data << "]\n}";
 
   memcpy(buf, ss.str().c_str(), ss.str().size());
@@ -63,19 +74,19 @@ Packet* json_to_packet(void* buf){
 
   json_object *id_user_json = json_object_object_get(raw_obj, "id_user");
   json_object *id_camera_json = json_object_object_get(raw_obj, "id_camera");
+  json_object *classes_json = json_object_object_get(raw_obj, "classe");
   json_object *timestamp_json = json_object_object_get(raw_obj, "timestamp");
   json_object *dados_json = json_object_object_get(raw_obj, "dados");
 
 
-  int id_user = (stoi(json_object_get_string(id_user_json)));
-  int id_camera = (stoi(json_object_get_string(id_camera_json)));
+  std::string id_user = (json_object_get_string(id_user_json));
+  std::string id_camera = (json_object_get_string(id_camera_json));
   // vamos ignorar isto por enquanto time_t timestamp =
   int arraylen = json_object_array_length(dados_json);
   vector<Mat> frames;
   Mat frame;
   json_object * medi_array_obj;
   for(int i = 0; i < arraylen; i++){
-
     medi_array_obj = json_object_array_get_idx(dados_json, i);
     string aux = json_object_get_string(medi_array_obj);
     aux += '\0';
@@ -86,11 +97,21 @@ Packet* json_to_packet(void* buf){
     json_object_put(medi_array_obj);
   }
 
+  vector<string> classes;
+  arraylen = json_object_array_length(classes_json);
+  for(int i = 0; i < arraylen; i++){
+    medi_array_obj = json_object_array_get_idx(classes_json, i);
+    string aux = json_object_get_string(medi_array_obj);
+    aux += '\0';
+    classes.push_back(aux);
+    json_object_put(medi_array_obj);
+  }
+
   //free
   json_object_put(id_user_json);
   json_object_put(id_camera_json);
   json_object_put(timestamp_json);
-  //json_object_put(dados_json);
+  json_object_put(classes_json);
 
-  return new Packet(id_user,id_camera,time(0),frames);
+  return new Packet(id_user,id_camera,time(0),frames, classes);
 }
--- server/src/packet.hpp
+++ server/src/packet.hpp
@@ -1,24 +1,27 @@
-
+#ifndef __FRAME_H
+#define __FRAME_H
 
 #include <opencv2/opencv.hpp>
 #include <ctime>
 
 
 const int FRAMES_PER_SEND = 100;
-const int JSON_BUFF_SIZE = 76800*2*100;
+const int JSON_BUFF_SIZE = 76800*2*FRAMES_PER_SEND;
 
 class Packet
 {
 public:
-  int id_user;
-  int id_camera;
+  std::string id_user;
+  std::string id_camera;
   std::time_t timestamp;
   std::vector<cv::Mat> frames;
-
+  std::vector<std::string> classes;
+  
   Packet();
-  Packet(int user, int camera, std::time_t ts, std::vector<cv::Mat> dados );
+  Packet(std::string user, std::string camera, std::time_t ts, std::vector<cv::Mat> dados, std::vector<std::string> dados_classes );
   ~Packet();
 };
 int packet_to_json(unsigned char* buf, Packet* pacote);
 Packet* json_to_packet(void* buf);
 
+#endif
